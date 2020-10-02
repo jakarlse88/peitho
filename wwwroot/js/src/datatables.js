23f1be0257cfa4c1b79e281c2854650a516aa91a
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-10-02 17:32:06 +0200

    Webpack for JS imports

---
+++ wwwroot/js/src/datatables.js
@@ -0,0 +1,14 @@
+﻿import {DataTable} from "simple-datatables";
+
+export function createTable(tableId) {
+    if (typeof(tableId) === 'undefined'
+        || tableId === null
+        || tableId.length === 0)
+    {
+        throw "Parameter 'tableId' was undefined, null, or empty.";
+    }
+    
+    const myTable = new DataTable(`{tableId}`);
+}
+
+    
---
+++ wwwroot/js/src/index.js
@@ -0,0 +1,16 @@
+﻿import {createTable} from "./datatables";
+
+export function CreateDataTable(tableId) {
+    if (typeof(tableId) === 'undefined' 
+        || tableId === null
+        || tableId.length === 0)
+    {
+        throw "Parameter 'tableId' was undefined, null, or empty.";
+    }
+    
+    createTable(tableId);
+}
+
+export function DestroyDataTable() {
+    console.log("asad");
+}
