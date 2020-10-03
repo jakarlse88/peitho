cd7eed459f536bb4b898b9245838acd4c60a3845
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-10-03 14:33:56 +0200

    Scaffold Technique>Create page

--- Pages/Admin/Technique/Index.razor.cs
+++ Pages/Admin/Technique/Index.razor.cs
@@ -1,27 +1,23 @@
 ﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Components;
-using Peitho.Models;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
+using Peitho.Models;
 using Peitho.Services;
 
-namespace Peitho.Pages.Admin
+namespace Peitho.Pages.Admin.Technique
 {
-    public partial class Technique
+    public partial class Index
     {
         [Inject] private IApiRequestService ApiRequestService { get; set; }
         [Inject] private IJSRuntime JsRuntime { get; set; }
         private IEnumerable<TechniqueModel> Techniques { get; set; }
         private const string TableId = "techniquesTable";
 
-        protected override async Task OnAfterRenderAsync(bool firstRender)
-        {
-            if (firstRender)
-            {
-                
-            }
-        }
-
+        /// <summary>
+        /// Component initialisation logic.
+        /// </summary>
+        /// <returns></returns>
         protected override async Task OnInitializedAsync()
         {
             const string requestUrl = "http://localhost:5002/api/technique/all";
